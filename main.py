e04171ba607d1eff76c890f20f277002aad79953
Bohdan Shcherba <73034554+bohdanshcherba@users.noreply.github.com>
2023-04-02 18:52:21 +0300

    create base

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ from pygame import *
 
 
 
+window = display.set_mode((700,500))
+
+
+game = True
+clock = time.Clock()
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    window.fill((200,10,20))
+
+    display.update()
+    clock.tick(60)
